7491b26c60f1b38cbf59e832e3dfcf11131c9235
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-08-01 18:11:35 -0300

    Criando a classe SistemaAmigo e a interface SistemaGenericoAmigoSecreto

---
+++ src/main/java/br/ufpb/dcx/laisa/amigoSecreto/SistemaAmigo.java
@@ -0,0 +1,14 @@
+package br.ufpb.dcx.laisa.amigoSecreto;
+
+import java.util.List;
+
+public class SistemaAmigo implements SistemaGenericoAmigoSecreto{
+    private List<Mensagem> mensagens;
+    private List<Amigo> amigos;
+
+
+    @Override
+    public void cadastraAmigo(String nome, String emailAmigo) {
+
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/laisa/amigoSecreto/SistemaGenericoAmigoSecreto.java
@@ -0,0 +1,7 @@
+package br.ufpb.dcx.laisa.amigoSecreto;
+
+public interface SistemaGenericoAmigoSecreto {
+    public void cadastraAmigo(String nome, String emailAmigo);
+
+
+}
