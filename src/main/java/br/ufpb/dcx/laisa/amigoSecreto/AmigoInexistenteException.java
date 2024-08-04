35635ccd999407e7bfebf5bbac9b5859c108adbb
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-08-04 00:50:14 -0300

    Criando as exceptions AmigoInexistenteException e AmigoNaoSorteadoException

---
+++ src/main/java/br/ufpb/dcx/laisa/amigoSecreto/AmigoInexistenteException.java
@@ -0,0 +1,7 @@
+package br.ufpb.dcx.laisa.amigoSecreto;
+
+public class AmigoInexistenteException extends Exception {
+    public AmigoInexistenteException(String msg){
+        super(msg);
+    }
+}
---
+++ src/main/java/br/ufpb/dcx/laisa/amigoSecreto/AmigoNaoSorteadoException.java
@@ -0,0 +1,7 @@
+package br.ufpb.dcx.laisa.amigoSecreto;
+
+public class AmigoNaoSorteadoException extends Exception {
+    public AmigoNaoSorteadoException(String msg){
+        super(msg);
+    }
+}
