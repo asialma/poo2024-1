97466b57c34aa665490a455ac0031a4c6d42e5c5
laisamariasantos@gmail.com <laisamariasantos@gmail.com>
2024-08-07 23:18:47 -0300

    Alterando SistemaBibliotecaCCAETest

--- src/test/java/br/ufpb/dcx/laisa/sistemaBiblioteca/SistemaBibliotecaCCAETest.java
+++ src/test/java/br/ufpb/dcx/laisa/sistemaBiblioteca/SistemaBibliotecaCCAETest.java
@@ -3,7 +3,6 @@ package br.ufpb.dcx.laisa.sistemaBiblioteca;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
