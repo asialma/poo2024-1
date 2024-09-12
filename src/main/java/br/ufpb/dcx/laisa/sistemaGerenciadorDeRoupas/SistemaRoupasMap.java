e640095368c85f598d1727a102b75b4811d5671f
laisa.maria <laisa.maria@dcx.ufpb.br>
2024-09-12 13:57:07 -0300

    Criando os métodos da classe SistemaRoupasMap

--- src/main/java/br/ufpb/dcx/laisa/sistemaGerenciadorDeRoupas/SistemaRoupasMap.java
+++ src/main/java/br/ufpb/dcx/laisa/sistemaGerenciadorDeRoupas/SistemaRoupasMap.java
@@ -1,10 +1,13 @@
 package br.ufpb.dcx.laisa.sistemaGerenciadorDeRoupas;
 
+import br.ufpb.dcx.laisa.sistemaBiblioteca.UsuarioJaExisteException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SistemaRoupasMap implements SistemaRoupas{
+public class SistemaRoupasMap implements SistemaRoupas {
 
     private Map<String, Roupa> roupas;
 
@@ -14,29 +17,52 @@ public class SistemaRoupasMap implements SistemaRoupas{
 
     @Override
     public void cadastrarRoupa(String descricao, TipoRoupa tipo, String[] cores, String tamanho) throws RoupaJaExisteException {
-
+        if (roupas.containsKey(descricao)) {
+            throw new RoupaJaExisteException("Roupa já cadastrada no sistema");
+        }
+        Roupa r = new Roupa(descricao, tipo, cores, tamanho);
+        roupas.put(descricao, r);
     }
 
     @Override
     public List<Roupa> pesquisarRoupasPorTipoETamanho(TipoRoupa tipo, String tamanho) {
-        return null;
+        List<Roupa> listaDeRoupasPorTipoETamanho = new ArrayList<>();
+        for (Roupa r : roupas.values()) {
+            if (r.getTipo().equals(tipo) && r.getTamanho().equals(tamanho)) {
+                listaDeRoupasPorTipoETamanho.add(r);
+            }
+        }
+        return listaDeRoupasPorTipoETamanho;
     }
 
     @Override
     public Roupa pesquisarRoupaPorDescricao(String descricao) throws RoupaNaoExisteException {
-        return null;
+        if (roupas.containsKey(descricao)) {
+            return roupas.get(descricao);
+        } else {
+            throw new RoupaNaoExisteException("Roupa não cadastrada no sistema");
+        }
     }
 
     @Override
     public boolean existeRoupaDoTipo(TipoRoupa tipo) {
+        for (Roupa r : roupas.values()) {
+            if (r.getTipo().equals(tipo)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public int contaRoupasDoTamanho(String tamanho) {
-        return 0;
+        int count = 0;
+        for (Roupa r : roupas.values()) {
+            if (r.getTamanho().equals(tamanho)) {
+                count++;
+            }
+        }
+        return count;
     }
 
-    // Demais métodos da classe considerando que implementa a interface e que lança em seus
-    // métodos as mesmas exceções dos métodos da interface.
 }
